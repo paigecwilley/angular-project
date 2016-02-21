0b7727327d67c4d1e297b333afcf968f4a3eecd5
Paige <paige.catherine.willey@gmail.com>
2016-02-20 23:49:48 -0700

    Day three commit

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,10 +1,25 @@
 angular.module('devMtIn').controller('homeCtrl', function($scope, profileService){
 
-$scope.myProfile = profileService.checkForProfile();
+// $scope.myProfile = profileService.checkForProfile();
 	// {
 	// friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name:'Zac'}, {name: 'Erin'}]
 	// };
 
+$scope.checkForProfile = function(){
+	var profileId = JSON.parse(localStorage.getItem('profileId'));
+
+	if (profileId){
+		profileService.checkForProfile(profileId.profileId)
+		.then(function(profile){
+			$scope.myProfile = profile.data;
+		})
+		.catch(function(err){
+			console.log(err);
+		});
+	}
+}
+
+$scope.checkForProfile();
 
 $scope.sortOptions = [{
 	display: 'Ascending'
@@ -25,9 +40,15 @@ $scope.saveProfile = function(profile) {
 	$scope.editing = false;
 }
 
-$scope.deleteProfile = function(profile) {
-	profileService.deleteProfile('profile');
-	$scope.myProfile = profileService.checkForProfile();
+$scope.deleteProfile = function() {
+	profileService.deleteProfile
+	.then(function(deleteProfile){
+		localStorage.removeItem('profileId');
+		$scope.myProfile = {};
+	})
+	.catch(function(err){
+		console.error(err);
+	});
 }
 
 });
--- profileService.js
+++ profileService.js
@@ -1,25 +1,50 @@
 angular.module('devMtIn')
-.service('profileService', function(){
+.service('profileService', function($http){
+
+var baseUrl = "http://connections.devmounta.in/";
+
 
 this.serviceTest = function() {
 	console.log('profileService is connected!');
 }
 
 this.saveProfile = function(profile) {
-	localStorage.setItem('profile', JSON.stringify(profile));
+	return $http({
+		method: 'POST',
+		url: baseUrl + 'api/profiles',/
+		data: profile
+	})
+	.then(function(profileResponse){
+		localStorage.setItem('profileID', JSON.stringify({profileID: profileResponse.data_id}));
+		console.log(profileResponse);
+	})
+	.catch(function(err){
+		console.error(err);
+	});
 }
 
-this.checkForProfile = function(){
-	if (localStorage.getItem('profile')){
-		return JSON.parse(localStorage.getItem('profile'));
-	}
-	return {
-		friends : [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
-	}
+this.checkForProfile = function(profileId){
+	return $http({
+		method: 'GET',
+		url: baseUrl + 'api/profiles' + profileId
+	});
+
+	// if (localStorage.getItem('profile')){
+	// 	return JSON.parse(localStorage.getItem('profile'));
+	// }
+	// return {
+	// 	friends : [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+	// }
 }
 
 this.deleteProfile = function(profile) {
-	localStorage.removeItem('profile');
+	// localStorage.removeItem('profile');
+	var profileId = JSON.parse(localStorage.getItem('profileId')).profileId;
+
+	return $http({
+		method: 'DELETE',
+		url: baseUrl +'api/profiles' + profileId
+	});
 }
 
 });
